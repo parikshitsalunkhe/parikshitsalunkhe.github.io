cb1a54c04af872841f3368b68d4ec8432354b167
Parikshit Salunkhe <salunkhe.parikshit@gmail.com>
2021-06-21 10:12:10 +0530

    Add files via upload

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Parikshit\Projects\Angular\parikshit-portfolio\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\side-projects\parikshit-portfolio\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
